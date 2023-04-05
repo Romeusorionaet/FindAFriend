a64790c22f028c4abf3ef22750a1ddf4a4678e0c
romeusorionaet <romeusoares14569@gmail.com>
2023-04-05 15:11:30 -0300

    header.3

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,26 @@ const button = document.getElementById('check_menu')
 
 const closeMenu = document.querySelector('.closeMenu')
 const openMenu = document.querySelector('.openMenu')
-
 const menu = document.querySelector('.nav_items')
+
+startScreenWidthCalculation();
+window.addEventListener('resize', startScreenWidthCalculation);
+
+function startScreenWidthCalculation(){
+    window.innerWidth >= 1200 ? 
+    menu.classList.remove('hidden')
+    : 
+    menu.classList.add('hidden')
+}
 
 button.addEventListener('click', ()=>{
 
     if(button.checked){
-        menu.classList.toggle('hidden')
+        menu.classList.remove('hidden')
         closeMenu.classList.toggle('hidden')
         openMenu.classList.toggle('hidden')
     }else{
-        menu.classList.toggle('hidden')
+        menu.classList.add('hidden')
         closeMenu.classList.toggle('hidden')
         openMenu.classList.toggle('hidden')
     }
